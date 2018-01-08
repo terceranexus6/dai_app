75481d40c87df24705483826ff8f92e4f83efe85
Paula <pauladelahoz@correo.ugr.es>
2018-01-08 07:39:16 -0500

    arreglo cosas

--- sitio_web/miapp/views.py
+++ sitio_web/miapp/views.py
@@ -2,5 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    context = {'boldmessage' : "I'm a bold message"}
-    return render(request, 'test/index.html', context)
+    context = {
+        'tittle' : "HAMLET",
+        'frase' : "Alas, he's mad!"
+    }
+    return render(request, 'test.html', context)
--- sitio_web/sitio_web/settings.py
+++ sitio_web/sitio_web/settings.py
@@ -118,5 +118,9 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/1.11/howto/static-files/
 
+#añado path, en lugar de hacerlo a pelo
+STATIC_PATH = os.path.join(BASE_DIR, 'static')
 STATIC_URL = '/static/'
-STATICFILES_DIRS = [os.path.join(BASE_DIR, 'static')]
+STATICFILES_DIRS = [
+    STATIC_PATH,
+]
