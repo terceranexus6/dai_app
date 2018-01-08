2a7fdba606b510a49dda6061cb71314e4be25e59
Paula <pauladelahoz@correo.ugr.es>
2018-01-08 07:59:46 -0500

    añado elementos dinamicos

--- sitio_web/miapp/views.py
+++ sitio_web/miapp/views.py
@@ -4,6 +4,7 @@ from django.http import HttpResponse
 def index(request):
     context = {
         'tittle' : "HAMLET",
-        'frase' : "Alas, he's mad!"
+        'frase' : "Alas, he's mad!",
+        'linkgithub' : "https://www.github.com/terceranexus6"
     }
     return render(request, 'test.html', context)
